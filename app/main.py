70d9e9f0cc8b866c8c07a5727b06e364802d8fcd
Junse0lee <ejrqlddl1@sch.ac.kr>
2026-02-11 11:03:55 +0900

    ready: 서버 배포를 위한 설정

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import face_analysis, hand_analysis
+from app.api import face_analysis, hand_analysis
 
 app = FastAPI()
 
